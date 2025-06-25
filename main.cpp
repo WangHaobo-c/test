372b078e5496f2b4ffcff6741638a9ebfcaff626
WangHaobo-c <2171398843@qq.com>
2025-06-25 15:57:55 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+template<class T>
+void Swap(T &a,T &b)
+{
+    T temp = a;
+
+    a = b;
+
+    b = temp;
+}
+
+int main()
+{
+    int a, b;
+    double x, y;
+    string str1, str2;
+    cin >> a >> b;
+    cin >> x >> y;
+    cin >> str1 >> str2;
+    Swap(a, b);
+    Swap(x, y);
+    Swap(str1, str2);
+    cout << a << " " << b << endl;
+    cout << x << " " << y << endl;
+    cout << str1 << " " << str2 << endl;
+    return 0;
+}
